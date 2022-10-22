1ed9fb946029bd9b4f6f8def5651f4f4b9f188d4
murdex5 <fkadupiige@gmail.com>
2022-10-22 12:58:02 +0200

    google authentication is added

--- creative-write/components/layout.js
+++ creative-write/components/layout.js
@@ -2,7 +2,7 @@ import Nav from './Nav';
 
 export default function Layout({ children }) {
     return (
-        <div className="mx-6 md:max-w-2x1 md-mx-auto bg-red-50 font-poppins">
+        <div className="mx-6 md:max-w-2x1 md-mx-auto font-poppins">
             < Nav />
             <main>{children}</main>
         </div>    
