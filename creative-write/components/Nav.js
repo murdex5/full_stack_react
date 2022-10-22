5345caed15e539f5272a10ebce4c083e093dbfd0
murdex5 <fkadupiige@gmail.com>
2022-10-22 11:56:15 +0200

    tailwind, next.js, firebase is added

---
+++ creative-write/components/Nav.js
@@ -0,0 +1,10 @@
+export default function Nav() {
+    return (
+        <div>
+            <h1>Nav</h1>
+        </div>
+
+    );
+};
+
+
---
+++ creative-write/components/layout.js
@@ -0,0 +1,10 @@
+import Nav from './Nav';
+
+export default function Layout({ children }) {
+    return (
+        <div className="mx-6 md:max-w-2x1 md-mx-auto bg-red-50 font-poppins">
+            < Nav />
+            <main>{children}</main>
+        </div>    
+    );
+};
